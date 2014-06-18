21a891cdddff06e4e21f17827b0d5d6641f81def
darrylsharp <meme-ing-of-life>
2014-06-18 19:52:19 -0400

    my first app

---
+++ app.js
@@ -0,0 +1,20 @@
+var express = require("express");
+var ejs = require("ejs");
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+app.engine('html', ejs.renderFile);
+
+app.get('/', function(req, res){
+	 console.log("req params", req.query)
+    res.render('index.ejs',
+      {
+            top: req.query.top,
+            bottom: req.query.bottom
+      });
+});
+
+app.listen(3000);
+console.log("Your app is running on Port 3000!");
+console.log("top thing", process.env.TOP)
+console.log("bottom thing"+ process.env.BOTTOM)
